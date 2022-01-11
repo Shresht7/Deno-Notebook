b5f3467637e9a70382a64c04167735d659dd5f09
Shresht Srivastav <Shresht7@users.noreply.github.com>
2022-01-12 02:06:55 +0530

    Create httpGetRequest

---
+++ manual/httpGetRequest.ts
@@ -0,0 +1,13 @@
+const url = Deno.args[0]    //  Get url from arguments
+
+//  Exit if invalid url
+if (!url) {
+    console.log('No url provided')
+    Deno.exit()
+}
+
+const res = await fetch(url)    //  Make fetch request
+
+const body = new Uint8Array(await res.arrayBuffer()) // read the response
+
+await Deno.stdout.write(body)   //  Write to stdout
