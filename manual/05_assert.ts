9869ecaa384581a1a35fd3dd0b44d5c3bdbeeada
Shresht Srivastav <Shresht7@users.noreply.github.com>
2022-01-13 01:18:55 +0530

    Create assert

---
+++ manual/05_assert.ts
@@ -0,0 +1,7 @@
+import { assertEquals } from 'https://deno.land/std@0.121.0/testing/asserts.ts'
+
+assertEquals('hello', 'hello')
+assertEquals(1, 1)
+assertEquals(true, true)
+
+console.log('Success!')
