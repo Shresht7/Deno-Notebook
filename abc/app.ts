1512f751e419ccb48836c5b5a109137bf919e35e
Shresht Srivastav <Shresht7@users.noreply.github.com>
2022-01-13 22:46:33 +0530

    Create abc/app.ts

---
+++ abc/app.ts
@@ -0,0 +1,13 @@
+import { Application } from 'https://deno.land/x/abc/mod.ts'
+
+const app = new Application()
+
+app.static('/', 'public')
+
+app.get('/', async (ctx) => {
+    await ctx.file('public/index.html')
+})
+
+const port = 8000
+console.log(`Server running on http://localhost:${port}`)
+app.start({ port })
