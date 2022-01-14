428ef115a56b8c4dbaef5593ac37f77047ba68fc
Shresht Srivastav <Shresht7@users.noreply.github.com>
2022-01-14 21:46:19 +0530

    Create examples

---
+++ examples/loggingWithColors.ts
@@ -0,0 +1,11 @@
+console.log('%cThis text is red', 'color: red')
+
+console.log('%cThis background is blue', 'background-color: blue')
+
+console.log('%cThis text is underlined', 'text-decoration: underline')
+
+console.log('%cThis text is striked through', 'text-decoration: line-through')
+
+console.log('%cThis text is bold', 'font-weight: bold')
+
+console.log('%cRed %cGreen %cBlue', 'color: red', 'color: green', 'color: blue')
