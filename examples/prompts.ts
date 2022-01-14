1161b5449cb4d290d2954c244aecb5c1130e0221
Shresht Srivastav <Shresht7@users.noreply.github.com>
2022-01-14 21:53:07 +0530

    Create prompts

---
+++ examples/prompts.ts
@@ -0,0 +1,12 @@
+alert('For your information')
+console.log('Information provided')
+
+const response = confirm('Are you sure? (y/n)')
+console.log('You chose', response)
+
+const name = prompt('Please enter your name:')
+console.log('Hello ', name)
+
+const age = prompt('Please enter your age:', '42')
+console.log('Hello ', age)
+
