8570cc57122e80b311c44f1e352782f3b7d188cf
Shresht Srivastav <Shresht7@outlook.com>
2022-01-16 16:36:57 +0530

    Create exec

---
+++ exec/mod.ts
@@ -0,0 +1,32 @@
+async function exec(...cmd: string[]) {
+
+    const process = Deno.run({
+        cmd,
+        stdout: 'piped',
+        stderr: 'piped',
+    })
+
+    const [status, stdout, stderr] = await Promise.all([
+        process.status(),
+        process.output(),
+        process.stderrOutput()
+    ])
+
+    if (status.code !== 0) {
+        const error = new TextDecoder().decode(stderr)
+        throw new Error(error)
+    }
+
+    return stdout
+
+}
+
+let stdout, text
+
+stdout = await exec('git', 'branch')
+text = new TextDecoder().decode(stdout)
+console.log('Current Git Branch:', text.match(/\*\s+(.*)/)?.[1])
+
+stdout = await exec('gh', 'api', 'zen')
+text = new TextDecoder().decode(stdout)
+console.log('Zen:', text)
